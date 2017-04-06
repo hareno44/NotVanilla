f591e7e0f8ac94b70e83248e1ec5421475af322c
Caroline Cata <caroline.cata@jrcisi.com>
2017-04-06 13:28:33 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+ $(document).ready(function() {
+   $('#toggle-nav').click(function(e) {
+        //  $(this).toggleClass('active');
+         $('#nav').toggleClass('active');
+
+        // e.preventDefault();
+    });
+ });
+
+// $('nav ul li').hide().removeClass('active');
+// $('nav li').hover(
+  // function () {
+    // $('ul', this).stop().slideDown(100);
+  // },
+  // function () {
+    // $('ul', this).stop().slideUp(100);
+  // }
+// );
